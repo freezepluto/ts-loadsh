b5210145f31b5aa964db9c818cc12e29452accb5
hongpc <danieleuoov@163.com>
2021-11-11 21:08:09 +0800

    fix(eq.ts): 移除多余的类型声明

--- packages/es/eq.ts
+++ packages/es/eq.ts
@@ -1,5 +1,4 @@
-let eq:(first:any,second:any)=>boolean
-
+let eq:(first,second)=>boolean
 eq = (first,second):boolean=>{
   //可能会存在两个NAN的情况，NAN 不等于自身
   return first === second || (first !== first) && (second !== second)
